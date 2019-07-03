0fde20b1df7f86e560022b935ffc103dd86c32d5
keybleach <diego301003@icloud.com>
2019-07-03 13:21:08 -0400

    created site

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.urlencoded({extended: false})
 
-const dummyData = [{taskItem: "Work on my portfolio" },{taskItem: "Code and watch anime"},{taskItem: "Sleep"}];
+const dummyData = [{title: "" , body: ""}];
 
 // setting up
 const app = express();
@@ -19,21 +19,43 @@ app.use(express.static('./public'));
 // Get for tasks: returns all tasks
 app.get('/', (req, res) => { 
     // rendering task view and passing task to do data 
-    res.render('tasks', {taskToDo: dummyData});
+    res.render('home', {taskToDo: dummyData});
 });
+
+app.get('/week1', (req, res) => { 
+    // rendering task view and passing task to do data 
+    res.render('week1', {taskToDo: dummyData});
+});
+
+app.get('/week2', (req, res) => { 
+    // rendering task view and passing task to do data 
+    res.render('week2', {taskToDo: dummyData});
+});
+
+app.get('/week3', (req, res) => { 
+    // rendering task view and passing task to do data 
+    res.render('week3', {taskToDo: dummyData});
+});
+
+app.get('/week4', (req, res) => { 
+    // rendering task view and passing task to do data 
+    res.render('week4', {taskToDo: dummyData});
+});
+
 
 // Post for tasks: posting a task
 app.post('/', urlEncoded, (req, res) =>{
 //   console.log("hitting Post route");
 // formating for incoming data to add to my data set
   let incomingItem = {}
-  incomingItem.taskItem = req.body.task
+  incomingItem.title = req.body.title
+  incomingItem.body= req.body.body;
   dummyData.push(incomingItem)
  console.log(dummyData)
-  res.redirect('/')
+  res.redirect('/');
 });
 // deleting specified task 
-app.delete("/tasks/:id", function(req, res){
+app.delete("/:id", function(req, res){
     // deleting item from data set 
     dummyData.splice(req.params.id, 1);
     // console.log(dummyData);
